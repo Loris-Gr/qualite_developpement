b7fbda901b37da7fd14e07c6f3058680cf16d804
Loris Grandchamp <loris.grandchamp@etu.univ-orleans.fr>
2024-02-08 11:16:25 +0100

    Création Journee et son Executable

---
+++ TP2/Auteur/Journee.java
@@ -0,0 +1,19 @@
+package Auteur ;
+public enum Journee {
+    MATIN,
+    APRESMIDI,
+    SOIREE;
+
+    public String toString() {
+    if (this == MATIN)
+	    return "Matin";
+	else if (this == APRESMIDI)
+	    return "Après-midi";
+	else {
+	    assert this == SOIREE;
+	    return "Soirée";
+	}
+    }
+    
+    
+}
